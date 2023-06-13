02a9a9d7b0a5bcf5a2de9d9709cdeb15d88a6ceb
Noah <noahkoleske@icloud.com>
2023-06-12 21:33:09 -0500

    while loop

--- game.js
+++ game.js
@@ -1,33 +1,42 @@
 
 var wins = 0;
-var loses = 0;
+var losses = 0;
 var ties = 0;
 
 var choices = ["R", "P", "S"]
 
-// player chooses R, P, S
-var humanChoice = prompt("Choose R, P, S")
- console.log(humanChoice)
-
-    // computer randomly chooses
-var randomNum = Math.floor(Math.random() * 3)
-console.log(choices[randomNum])
+var keepPlaying = true
 
 
-// compare choices
-    // if humanChoice is R and computerChoise is S OR
-    // if humanChoice is P and computerChoise is R OR
-    // if humanChoice is S and computerChoise is P OR
-        // alert that we won
-        // increse wins
-    // if humanChoise === computerChoice
-        // alert that we tied
-    // else
-        // alert that we lost
-        // inscrease loses
+while (keepPlaying) {
+    // player chooses R, P, S
+    var humanChoice = prompt("Choose R, P, S")
 
-// ask user "do you want to play again?"
-    // if yes
-        // restart game
-    // if no
-        // exit game
+
+        // computer randomly chooses
+    var randomNum = Math.floor( Math.random() * 3 )
+    var computerChoice = choices[randomNum]
+
+
+    if (
+        humanChoice === "S" && computerChoice === "P" ||
+        humanChoice === "R" && computerChoice === "S" ||
+        humanChoice === "P" && computerChoice === "R" 
+    ) {
+        alert("You won!")
+        wins++
+    } else if (humanChoice === computerChoice) {
+        alert("Round tied!")
+        ties++
+    } else {
+        alert("You lost!")
+        losses++
+    }
+
+    alert("Wins: " + wins + "\nTies: " + ties + "\nLosses: " + losses)
+    
+    
+    keepPlaying = confirm("Want to play again?")
+}
+
+alert("Play again soon!")
