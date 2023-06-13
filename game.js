6423a3f1eb413f55bbc124292bb719d720140107
Noah <noahkoleske@icloud.com>
2023-06-12 20:58:17 -0500

    humanChoice

--- game.js
+++ game.js
@@ -1,1 +1,33 @@
-console.log("Hello RPS!")
+
+var wins = 0;
+var loses = 0;
+var ties = 0;
+
+var choices = ["R", "P", "S"]
+
+// player chooses R, P, S
+var humanChoice = prompt("Choose R, P, S")
+ console.log(humanChoice)
+
+    // computer randomly chooses
+var randomNum = Math.floor(Math.random() * 3)
+console.log(choices[randomNum])
+
+
+// compare choices
+    // if humanChoice is R and computerChoise is S OR
+    // if humanChoice is P and computerChoise is R OR
+    // if humanChoice is S and computerChoise is P OR
+        // alert that we won
+        // increse wins
+    // if humanChoise === computerChoice
+        // alert that we tied
+    // else
+        // alert that we lost
+        // inscrease loses
+
+// ask user "do you want to play again?"
+    // if yes
+        // restart game
+    // if no
+        // exit game
